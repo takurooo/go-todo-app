1c54b7c3a809c4fff497e2838ee046bed4c264ab
takurooo <35373553+takurooo@users.noreply.github.com>
2024-03-02 23:57:56 +0900

    SECTION-87

--- service/add_task.go
+++ service/add_task.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/takurooo/go-todo-app/auth"
 	"github.com/takurooo/go-todo-app/entity"
 	"github.com/takurooo/go-todo-app/store"
 )
@@ -14,7 +15,12 @@ type AddTask struct {
 }
 
 func (a *AddTask) AddTask(ctx context.Context, title string) (*entity.Task, error) {
+	id, ok := auth.GetUserID(ctx)
+	if !ok {
+		return nil, fmt.Errorf("user_id not found")
+	}
 	t := &entity.Task{
+		UserID: id,
 		Title:  title,
 		Status: entity.TaskStatusTodo,
 	}
